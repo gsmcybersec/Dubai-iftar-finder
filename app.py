9b028bd3811771cd4f2e49b9231bd0745ce70210
gsmcybersec <technogsmfarooq@gmail.com>
2026-02-26 13:26:02 +0400

    Update app.py

--- app.py
+++ app.py
@@ -548,9 +548,8 @@ def _get_food_types(conn):
         for t in row[0].split(','):
             types.add(t.strip())
     return sorted(types)
+init_db()
 
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
-    
